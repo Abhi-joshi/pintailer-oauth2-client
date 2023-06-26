e6b0c044e83325fbfa2f5b33cdd6a1d1929fe389
Abhishek Sharma <mail.abhishekjoshi@gmail.com>
2023-06-26 19:48:26 +0530

    Adding README.md file

--- src/main/java/com/faberwork/pintaileroauth2client/config/SecurityConfig.java
+++ src/main/java/com/faberwork/pintaileroauth2client/config/SecurityConfig.java
@@ -7,10 +7,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
 import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -31,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public ServerOAuth2AuthorizationRequestResolver serverOAuth2AuthorizationRequestResolver(ReactiveClientRegistrationRepository repo) {
         DefaultServerOAuth2AuthorizationRequestResolver resolver = new DefaultServerOAuth2AuthorizationRequestResolver(repo);
-        //resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
+        resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
         return resolver;
     }
 
